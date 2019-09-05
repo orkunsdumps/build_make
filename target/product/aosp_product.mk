b380769b13620d3a38e4c7a8c0d7b1a52626fbd9
dianlujitao <dianlujitao@lineageos.org>
2019-09-05 21:56:36 +0800

    product: Exclude AOSP apn list on Halcyon build Change-Id: I1fd10089421a2e4b57ef565d87ad1f6036bd1898
    Signed-off-by: HeroBuxx <me@buxxed.tk>

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -30,5 +30,7 @@ PRODUCT_PACKAGES += \
 
 # Telephony:
 #   Provide a APN configuration to GSI product
+ifeq ($(HALCYON_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
